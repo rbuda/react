398ac2d1ffb02d6f59ee0cd2e042a6282e1967a5
Mike Verthein <mike.verthein@gmail.com>
2016-05-14 14:17:47 -0500

    map with current location

--- index.ios.js
+++ index.ios.js
@@ -1,83 +1,175 @@
-import React, {
-  Component
-} from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  MapView,
-  Text,
-  View
-} from 'react-native';
+var React = require('react-native');
 
-var Api = require('./src/api');
+var {
+  AppRegistry,
+  Image,
+  PropTypes,
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  MapView,
+  AlertIOS,
+} = React;
+
+var {width, height} = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 41.889357;
+const LONGITUDE = -87.637604;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 var Weather = React.createClass({
-  getInitialState: function() {
+  watchID: (null: ?number),
+  getInitialState() {
     return {
-      pin: {
-      latitude: 0,
-      longitude: 0
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
       },
-    city: '',
-    temperature: '',
-    description: ''
     };
   },
-  //   var pins = [{
-  //     latitude: 41.85,
-  //     longitude: -87.75
-  //   }];
 
-  render: function() {
-    return <View style={styles.container}>
-      <MapView
-        annotations={[this.state.pin]}
-        onRegionChangeComplete={this.onRegionChangeComplete}
-        style={styles.map}>
-      </MapView>
-      <View style={styles.textWrapper}>
-        <Text style={styles.text}>{this.state.city}</Text>
-        <Text style={styles.text}>{this.state.temperature}</Text>
-        <Text style={styles.text}>{this.state.description}</Text>
-      </View>
-    </View>
-  },
-  onRegionChangeComplete: function(region) {
-    this.setState({
-      pin: {
-        longitude: region.longitude,
-        latitude: region.latitude
+  componentDidMount() {
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = {
+          long: parseFloat(position.coords.longitude),
+          lat: parseFloat(position.coords.latitude)
+        }
+        this.setState({
+          initialPosition
+        })
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 5000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = {
+        long: parseFloat(position.coords.longitude),
+        lat: parseFloat(position.coords.latitude)
       }
-    });
-
-    // this === component
-    Api(region.latitude, region.longitude)
-      .then((data) => {
-        console.log(data);
-        this.setState(data);
+      this.setState({
+        lastPosition
       });
-    // console.log(region);
-  }
+    });
+  },
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  },
+
+  onRegionChange(region) {
+    this.setState({ region });
+  },
+
+  updateMarkerCoordinate(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers[0],
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: id++,
+          color: 'rgba(236,64,122,1)',
+        },
+      ],
+    })
+    console.log(this.state.markers[0].key)
+    console.log(this.state.markers)
+  },
+
+  goToRequest(marker) {
+    this.props.navigator.push({
+      navigationBarHidden: true,
+      component: RequestDetail,
+      passProps: {requestKey: marker.requestKey, requestCoordinate: marker.coordinate, requestDescription: marker.description}
+    });
+  },
+
+  goBack() {
+    this.props.navigator.pop();
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref="map"
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+          showsUserLocation={true}
+        >
+        </MapView>
+      </View>
+    );
+  },
 });
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5CFFF'
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    marginTop: 20,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    flex: 2,
-    marginTop: 30
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
-  textWrapper: {
-    flex: 1,
-    alignItems: 'center'
+  callout: {
+    backgroundColor: 'rgba(236,64,122,0.7)',
+  },
+  bubble: {
+    backgroundColor: 'rgba(236,64,122,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 140,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+  avatar1: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+  },
+  avatar2: {
+    position: 'absolute',
+    top: 5,
+    left: 5,
+  },
+  avatar: {
+    borderRadius: 25,
+    width: 50,
+    height: 50
   },
   text: {
-    fontSize: 30
-  }
+    color: 'white',
+  },
 });
-
 AppRegistry.registerComponent('weather', () => Weather);
